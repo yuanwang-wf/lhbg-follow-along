269fb6a6b5c23c51dd3b25d4a84da03e5f70a406
yuanwang <yuan.wang@workiva.com>
2022-04-23 14:19:19 -0600

    wip

--- flake.nix
+++ flake.nix
@@ -1,8 +1,13 @@
 {
   # inspired by: https://serokell.io/blog/practical-nix-flakes#packaging-existing-applications
   description = "A Hello World in Haskell with a dependency and a devShell";
-  inputs.nixpkgs.url = "nixpkgs";
-  outputs = { self, nixpkgs }:
+
+ inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs , flake-utils }:
     let
       supportedSystems = [ "x86_64-linux" "x86_64-darwin" ];
       forAllSystems = f: nixpkgs.lib.genAttrs supportedSystems (system: f system);
