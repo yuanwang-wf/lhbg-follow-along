28bc6489b57e0763694c9af3eedbad220dd0b11c
yuanwang <yuan.wang@workiva.com>
2022-05-01 08:38:35 -0600

    3.5 modulizing

--- Main.hs
+++ Main.hs
@@ -1,29 +1,18 @@
 module Main where
 
+import Html
+
 main :: IO ()
-main = putStrLn $ makeHtml "My page title" "My page content"
+main = putStrLn (render myhtml)
 
-makeHtml :: String -> String -> String
-makeHtml title content = html_  (head_ (title_ title) <> body_ ( h1_ (p_ content)))
-
-html_ :: String -> String
-html_ = el "html"
-
-head_ :: String -> String
-head_ = el "head"
-
-title_ :: String -> String
-title_ = el "title"
-
-body_ :: String -> String
-body_ = el "body"
-
-p_ :: String -> String
-p_ = el "p"
-
-h1_ :: String -> String
-h1_ = el "h1"
-
-el :: String -> String -> String
-el tag content =
-  "<" <> tag <> ">" <> content <> "</" <> tag <> ">"
+myhtml :: Html
+myhtml =
+  html_
+    "My title"
+    ( append_
+        (h1_ "Heading")
+        ( append_
+            (p_ "Paragraph #1")
+            (p_ "Paragraph #2")
+        )
+    )
